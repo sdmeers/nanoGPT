77802485aa5655c1e3e756d57c24dc2c1701c2fe
Steven Meers <stevenmeers@gmail.com>
2025-08-04 20:51:53 +0100

    Added training on Sherlock Holmes data

--- config/finetune_holmes.py
+++ config/finetune_holmes.py
@@ -1,14 +1,14 @@
 import time
 
-out_dir = 'out-shakespeare'
+out_dir = 'out-holmes'
 eval_interval = 5
 eval_iters = 40
 wandb_log = False # feel free to turn on
-wandb_project = 'shakespeare'
+wandb_project = 'holmes'
 wandb_run_name = 'ft-' + str(time.time())
 
-dataset = 'shakespeare'
-init_from = 'gpt2' # 'gpt2-xl' # this is the largest GPT-2 model
+dataset = 'holmes'
+init_from = 'gpt2-xl' # this is the largest GPT-2 model
 
 # only save checkpoints if the validation loss improves
 always_save_checkpoint = False
